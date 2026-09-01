0eb7e2286d5d4d9e49b128b92411b2b93f050c16
Aksh Choudhary <289365507+akshchoudhary20121-sys@users.noreply.github.com>
2026-09-01 12:02:55 +0000

    Sync uncommitted changes from freebuff.com

---
+++ isolate/assets/message-square-GlupCbCg.js
@@ -0,0 +1,1 @@
+import{c}from"./index-1pKmvBZl.js";const e=[["path",{d:"M12 6v6l4 2",key:"mmk7yg"}],["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}]],s=c("clock",e);const a=[["path",{d:"M22 17a2 2 0 0 1-2 2H6.828a2 2 0 0 0-1.414.586l-2.202 2.202A.71.71 0 0 1 2 21.286V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2z",key:"18887p"}]],t=c("message-square",a);export{s as C,t as M};
